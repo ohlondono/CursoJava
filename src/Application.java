e7bb576bacf0ccb622bd3b06f2db102fb87a152a
oscarlondono <oscarlondono@simplesolutions.com.co>
2017-02-28 15:59:14 -0500

    First Commit for this branch

--- src/Application.java
+++ src/Application.java
@@ -8,7 +8,11 @@ public class Application {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		System.out.println("Hello Java World");
+		/*System.out.println("Hello Java World");*/
+		int myNumber;
+		myNumber = 77;
+		System.out.println(myNumber);
+		
 	}
 
 }
