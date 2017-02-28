49135c7dfd6773a5132673db5b98b14745538959
Oscar <ohlondono@gmail.com>
2017-02-28 16:23:26 -0500

    Second Commit

--- src/Application.java
+++ src/Application.java
@@ -8,7 +8,6 @@ public class Application {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		/*System.out.println("Hello Java World");*/
 		int myNumber;
 		myNumber = 77;
 		System.out.println(myNumber);
