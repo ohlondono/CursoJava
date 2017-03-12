1058fc99313b1239dfe14c4d15957e12c8a4bbc6
ohlondono <ohlondono@gmail.com>
2017-03-12 08:12:15 -0500

    Tutorial 3 y 4

--- src/Application.java
+++ src/Application.java
@@ -9,13 +9,23 @@ public class Application {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		int myNumber;
-		myNumber = 77;
-		System.out.println(myNumber);
-		String myString;
-		myString = "Julio";
-		System.out.println(myString);
+		int myNumber = 77;
+		short myShort = 7;
+		long myLong = 2016;
+		double myDouble = 7.716;
+		float myFloat = 771.6f;
+		char myChar = 'A';
+		boolean myBoolean = true;
+		byte myByte = 77;
 		
+		System.out.println(myNumber);
+		System.out.println(myShort);
+		System.out.println(myLong); 
+		System.out.println(myDouble);
+		System.out.println(myFloat);
+		System.out.println(myChar);
+		System.out.println(myBoolean);
+		System.out.println(myByte);
 	}
 
 }
---
+++ src/StringOperations.java
@@ -0,0 +1,27 @@
+/**
+ * @author Oscar
+ *
+ */
+public class StringOperations {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int myInt = 7;
+		String text = "Hello";
+		String blank = " ";
+		String name = "Panda";
+		String greeting = text + blank + name;
+		
+		System.out.println(greeting);
+		
+		System.out.println("Hello" + " "  + "Panda");
+
+		System.out.println("My age is: " + myInt);
+		
+		double myDouble = 7.716;
+		System.out.println("My number is: " + myDouble);
+	}
+
+}
