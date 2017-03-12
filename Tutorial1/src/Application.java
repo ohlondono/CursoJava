3f5a0adeda486f072f515ac967254de87f681d20
ohlondono <ohlondono@gmail.com>
2017-03-12 08:27:30 -0500

    RefactorCurso
    Renombrar Proyectos

---
+++ Tutorial1/src/Application.java
@@ -0,0 +1,31 @@
+/**
+ * @author ohlo
+ *
+ */
+public class Application {
+
+	/**
+	 * Main Method.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int myNumber = 77;
+		short myShort = 7;
+		long myLong = 2016;
+		double myDouble = 7.716;
+		float myFloat = 771.6f;
+		char myChar = 'A';
+		boolean myBoolean = true;
+		byte myByte = 77;
+		
+		System.out.println(myNumber);
+		System.out.println(myShort);
+		System.out.println(myLong); 
+		System.out.println(myDouble);
+		System.out.println(myFloat);
+		System.out.println(myChar);
+		System.out.println(myBoolean);
+		System.out.println(myByte);
+	}
+
+}
---
+++ Tutorial4/src/Application.java
@@ -0,0 +1,17 @@
+
+public class Application {
+	/*
+	 * Writing "main" + ctrl + esp it will create the main method.
+	 * Also with ctrl + shift + F, it will format the code (e.g. indent) 
+	 */
+	
+	public static void main(String[] args) {
+		int value = 0;
+		
+		while (value < 20) {
+			System.out.println("Hello " + value);
+			value = value + 1;
+		}
+	}
+}
+ 
